02f0d1b09830fc88af0a24c6e3fe72d54fb0526a
c4droid <c4droid@foxmail.com>
2018-02-18 11:41:18 +0800

    Recover project,using multiboot bootloader written by C/C++

--- src/kernel/idt/idt.c
+++
@@ -1,9 +0,0 @@
-#include "idt.h"
-#include "system.h"
-void set_idt_gate(unsigned char num,unsigned long base,unsigned short selector,unsigned char flags) {}
-void init_idt() {
-    iptr.limit = (sizeof(struct idt_entry) * 256) - 1;
-    iptr.base = &idt;
-    memset(&idt,0,sizeof(struct idt_entry) * 256);
-    idt_load();
-}
