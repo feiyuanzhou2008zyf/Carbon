1ff04c514f34f3bff742055dcdaa96552ce5e94c
c4droid <c4droid@foxmail.com>
2018-03-04 11:37:53 +0800

    Test Physic Memory API

--- src/include/physic_memory.h
+++ src/include/physic_memory.h
@@ -1,7 +1,16 @@
 #ifndef PHYSIC_MEMORY_H
 #define PHYSIC_MEMORY_H
 #include "multiboot.h"
+#define STACK_SIZE 8192
+#define PHYSIC_MEMORY_MAX_SIZE 0x20000000
+#define PHYSIC_MEMORY_PAGE_SIZE 0x1000
+#define PAGE_MAX_SIZE (PHYSIC_MEMORY_MAX_SIZE / PHYSIC_MEMORY_PAGE_SIZE)
+#define PHYSIC_PAGE_MASK 0xFFFFF000
 extern uint8_t kernel_start[];
 extern uint8_t kernel_end[];
+extern uint32_t physic_page_count;
 void show_memory_map();
+void init_physic_page();
+uint32_t physic_alloc_page();
+void physic_free_page(uint32_t p);
 #endif // PHYSIC_MEMORY_H
--- src/kernel.c
+++ src/kernel.c
@@ -16,5 +16,17 @@ int kernel_main() {
     printk("kernel in memory end:   0x%08X\n", kernel_end);
     printk("kernel in memory used:   %d KB\n\n", (kernel_end - kernel_start + 1023) / 1024);
 	show_memory_map();
+	init_physic_page();
+	printk("\nThe Count of Physical Memory Page is: %u\n\n", physic_page_count);
+    uint32_t alloc_addr = NULL;
+    printk("Test Physical Memory Alloc :\n");
+    alloc_addr = physic_alloc_page();
+    printk("Alloc Physical Addr: 0x%08X\n", alloc_addr);
+    alloc_addr = physic_alloc_page();
+    printk("Alloc Physical Addr: 0x%08X\n", alloc_addr);
+    alloc_addr = physic_alloc_page();
+    printk("Alloc Physical Addr: 0x%08X\n", alloc_addr);
+    alloc_addr = physic_alloc_page();
+    printk("Alloc Physical Addr: 0x%08X\n", alloc_addr);
 	return 0;
 }
