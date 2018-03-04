64662370bd9b6de5a4cec953a81594c33a2fedd3
c4droid <c4droid@foxmail.com>
2018-03-04 21:37:59 +0800

    Update repo

--- src/kernel.c
+++ src/kernel.c
@@ -54,6 +54,7 @@ void kernel_main() {
     printk("Alloc Physical Addr: 0x%08X\n", alloc_addr);
     alloc_addr = physic_alloc_page();
     printk("Alloc Physical Addr: 0x%08X\n", alloc_addr);
+	print_cur_status();
 	test_heap();
 	while (1) {
 		asm volatile ("hlt");
