abdc794c347ff864e0aa772fe7686624269c2ce9
c4droid <c4droid@foxmail.com>
2018-03-04 10:28:37 +0800

    Rewrite IDT,Add new API for IDT

--- src/kernel.c
+++ src/kernel.c
@@ -2,10 +2,14 @@
 #include "stdio.h"
 #include "debug.h"
 #include "gdt.h"
+#include idt.h
 int kernel_main() {
 	init_debug();
 	init_gdt();
+	init_idt();
 	console_clear();
 	printk("Hello World,Here Kernel\n");
+	asm volatile ("int $0x3");
+	asm volatile ("int $0x4");
 	return 0;
 }
