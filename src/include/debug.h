765cceff707b07fd9cd31dfe5d1f90069e6d8e25
c4droid <c4droid@foxmail.com>
2018-03-05 21:10:44 +0800

    Update macro: assert defintion

--- src/include/debug.h
+++ src/include/debug.h
@@ -3,15 +3,20 @@
 #include "console.h"
 #include "varg.h"
 #include "elf.h"
-#define assert(x, info)	\
-    do { \
-        if (!(x)) {	\
-            panic(info); \
-        } \
-    } while (0)
-#define static_assert(x) \
-	switch (x) { case 0: case x: ; }
+#include "stdio.h"
+#define PANIC(...) panic(__FILE__,__LINE__,__FUNC__,__VA_ARGS__)
+#define static_assert(x) switch (x) { case 0: case x: ; }
+#ifdef NDEBUG
+	#define assert(CONDITION) ((void)0)
+#else
+	#define assert(CONDITION) if (CONDITION) {} else { PANIC(CONDITION); }
+#endif // NDEBUG
 void init_debug();
 void panic(const char *msg);
 void print_cur_status();
+void panic(char *filename,int line,const char *func,const char *condition) {
+	printk("\n\n\nError!\n");
+	printk("Filename: %s\n,Line: %d\n,Function: %s\n,Condition: %s\n",filename,line,(char *)func,(char *)condition);
+	while (1) { asm volatile ("hlt"); }
+}
 #endif // DEBUG_H
