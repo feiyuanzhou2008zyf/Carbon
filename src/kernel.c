8f53bff557cec09d0e2828fada0c705bf488ad07
wangchen11 <1012371864@qq.com>
2018-03-14 09:47:17 +0800

    kernel.c extern global multiboot_ptr from boot.c.

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,12 @@
 #include "kernel.h"
 int memory_size;
 void kernel_main();
-multiboot_t *global_multiboot_ptr_tmp;
+extern multiboot_t *multiboot_ptr;
 multiboot_t *global_multiboot_ptr;
 extern uint8_t kernel_start[];
 extern uint8_t kernel_end[];
 __attribute__((section(".text"))) void kernel_entry() {
-	global_multiboot_ptr = global_multiboot_ptr_tmp;
+	global_multiboot_ptr = multiboot_ptr;
 	memory_size = global_multiboot_ptr -> mem_upper;
 	kernel_main();
 	asm volatile ("hlt");
