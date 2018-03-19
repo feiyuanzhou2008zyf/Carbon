7f422bf3520c4315a1fb156ad8b0d96d6a2ba942
c4droid <c4droid@foxmail.com>
2018-03-19 13:38:30 +0800

    Add GDT Descriptor,Ready to test

---
+++ src/descriptor/global.c
@@ -0,0 +1,27 @@
+#include "global.h"
+gdt_entry_t entrys[5];
+gdt_ptr_t gdt_pointer;
+static gdt_entry_t create_gdt(uint32_t base, uint32_t limit, uint16_t flag);
+static gdt_entry_t create_gdt(uint32_t base, uint32_t limit, uint16_t flag) {
+	uint64_t entry;
+	entry = limit & 0x000F0000;
+	entry |= (flag << 8) & 0x00F0FF00;
+	entry |= (base >> 16) & 0x000000FF;
+	entry |= base & 0xFF000000;
+	entry <<= 32;
+	entry |= base << 16;
+	entry |= limit & 0x0000FFFF;
+	return entry;
+}
+void init_gdt() {
+	gdt_pointer.limit = (sizeof(gdt_entry_t) * 5) - 1;
+	gdt_pointer.base = (uint32_t)entrys;
+	entrys[0] = create_entry(0, 0, 0);
+	entrys[1] = create_entry(0, 0x000FFFFF, (GDT_CODE_PL0));
+	entrys[2] = create_entry(0, 0x000FFFFF, (GDT_DATA_PL0));entrys[3] = create_entry(0, 0x000FFFFF, (GDT_CODE_PL3));
+	entrys[4] = create_entry(0, 0x000FFFFF, (GDT_DATA_PL3));
+	flush_gdt((uint32_t)&gdt_pointer);
+}
+void flush_gdt(gdt_entry_t ptr) {
+	asm volatile ("lgdt %0" : : "m"(ptr));
+}
---
+++ src/include/global.h
@@ -0,0 +1,40 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+#define SEG_DESCTYPE(x) ((x) << 0x04)
+#define SEG_PRES(x) ((x) << 0x07)
+#define SEG_SAVL(x) ((x) << 0x0C)
+#define SEG_LONG(x) ((x) << 0x0D)
+#define SEG_SIZE(x) ((x) << 0x0E)
+#define SEG_GRAN(x) ((x) << 0x0F)
+#define SEG_PRIV(x) (((x) & 0x03) << 0x05)
+#define SEG_DATA_RD 0x00
+#define SEG_DATA_RDA 0x01
+#define SEG_DATA_RDWR 0x02
+#define SEG_DATA_RDWRA 0x03
+#define SEG_DATA_RDEXPD 0x04
+#define SEG_DATA_RDEXPDA 0x05
+#define SEG_DATA_RDWREXPD 0x06
+#define SEG_DATA_RDWREXPDA 0x07
+#define SEG_CODE_EX 0x08
+#define SEG_CODE_EXA 0x09
+#define SEG_CODE_EXRD 0x0A
+#define SEG_CODE_EXRDA 0x0B
+#define SEG_CODE_EXC 0x0C
+#define SEG_CODE_EXCA 0x0D
+#define SEG_CODE_EXRDC 0x0E
+#define SEG_CODE_EXRDCA 0x0F
+#define GDT_CODE_PL0 (SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | SEG_LONG(0) | SEG_SIZE(1) | SEG_GRAN(1) | SEG_PRIV(0) | SEG_CODE_EXRD)
+#define GDT_DATA_PL0 (SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | SEG_LONG(0) | SEG_SIZE(1) | SEG_GRAN(1) | SEG_PRIV(0) | SEG_DATA_RDWR)
+#define GDT_CODE_PL3 (SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | SEG_LONG(0) | SEG_SIZE(1) | SEG_GRAN(1) | SEG_PRIV(3) | SEG_CODE_EXRD)
+#define GDT_DATA_PL3 (SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | SEG_LONG(0) | SEG_SIZE(1) | SEG_GRAN(1) | SEG_PRIV(3) | SEG_DATA_RDWR)
+#include "stddef.h"
+struct gdt_ptr {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
+typedef struct gdt_ptr gdt_ptr_t;
+typedef uint64_t gdt_entry_t;
+void init_gdt();
+void flush_gdt(gdt_entry_t ptr);
+gdt_entry_t create_gdt(uint32_t base,uint32_t limit,uint16_t flag);
+#endif // GLOBAL_H
