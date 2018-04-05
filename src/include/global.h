8fa0d06ee136698425b8d05f761d3dd9c609bfc8
c4droid <c4droid@foxmail.com>
2018-04-05 16:56:30 +0800

    Add flush gdt C/C++ function

--- src/include/global.h
+++ src/include/global.h
@@ -1,7 +1,6 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 #include "stddef.h"
-#define GDT_NUM 256
 typedef struct gdt_entry_t {
 	uint16_t limit_low;
 	uint16_t base_low;
@@ -15,5 +14,5 @@ typedef struct gdt_ptr_t {
 	uint32_t base;
 } __attribute__((packed)) gdt_ptr_t;
 void init_gdt();
-extern void flush_gdt(gdt_entry_t *);
+extern void flush_gdt(uint32_t);
 #endif // GLOBAL_H
