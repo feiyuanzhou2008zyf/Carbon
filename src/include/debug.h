65036800e286530e2e4b53ff75530d55f2ea8394
c4dr01d <c4droid@foxmail.com>
2018-04-07 12:35:39 +0800

    Delete unused macro,and add -std=gnu11 compile options

--- src/include/debug.h
+++ src/include/debug.h
@@ -9,18 +9,6 @@
 #else
 #define assert(expr) ((void)0)
 #endif // NDEBUG
-#ifndef static_assert
-#define static_assert(expr) ((void)sizeof(char[1 - 2 * !!!(expr)]))
-#endif // static_assert
-#ifndef offsetof
-#define offsetof(type,number) ((size_t) & ((type*)0 -> number))
-#endif // offsetof
-#ifndef container_of
-#define container_of(ptr,type,member) ((type *)((char *)(ptr) - offsetof(type,member)))
-#endif // container_of
-#ifndef array_size
-#define array_size(arr) (sizeof(arr) / sizeof((arr)[0]) + sizeof(typeof(int[1 - 2 * !!__builtin_types_compatible_p(typeof(arr),typeof(&arr[0]))])) * 0)
-#endif // array_size
 void _assert(void *exp, void *file, unsigned int line);
 void init_debug();
 void panic(const char *msg);
